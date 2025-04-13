8c9336b98a1b5381ddc27dd76af1778baa1cc400
Tushar Kaul <tusharkaul9@gmail.com>
2025-04-13 07:36:51 +0530

    env

--- app/api/users/verify/route.ts
+++ app/api/users/verify/route.ts
@@ -3,32 +3,39 @@ import axios from "axios";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
-  const body = await request.json();
+	const body = await request.json();
 
-  try {
-    const apiResponse = await axios.post(
-      "http://ec2-3-28-58-24.me-central-1.compute.amazonaws.com/api/v1/auth/verify-token",
-      body
-    );
-    return NextResponse.json({
-      data: apiResponse.data,
-      code: apiResponse.status,
-    });
-  } catch (error: unknown) {
-    if (typeof error === "object" && error !== null && "response" in error) {
-      const errResponse = error as {
-        response?: { data?: JSON; status?: number };
-      };
+	console.log(`${process.env.API_RESPONSE_URL}/auth/verify-token`);
 
-      return NextResponse.json({
-        message: errResponse.response?.data || "Verify failed",
-        code: errResponse.response?.status || 500,
-      });
-    }
+	try {
+		const apiResponse = await axios.post(
+			`${process.env.API_RESPONSE_URL}/auth/verify-token`,
+			body
+		);
+		return NextResponse.json({
+			data: apiResponse.data,
+			code: apiResponse.status,
+		});
+	} catch (error: unknown) {
+		console.error("Verification error details:", error);
+		if (
+			typeof error === "object" &&
+			error !== null &&
+			"response" in error
+		) {
+			const errResponse = error as {
+				response?: { data?: JSON; status?: number };
+			};
 
-    return NextResponse.json({
-      message: "An unknown error occurred",
-      code: 500,
-    });
-  }
+			return NextResponse.json({
+				message: errResponse.response?.data || "Verify failed",
+				code: errResponse.response?.status || 500,
+			});
+		}
+
+		return NextResponse.json({
+			message: "An unknown error occurred",
+			code: 500,
+		});
+	}
 }
